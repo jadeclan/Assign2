2bf655128edda6dcb3c38b4525fa22872055cdd2
jadeclan <jberg475@mtroyal.ca>
2016-03-21 18:58:16 -0600

    Book List Start - need to tie in to dashboard

--- public/controllers/books.js
+++ public/controllers/books.js
@@ -2,6 +2,10 @@
  * Created by Nolan Walker on 2016-03-14.
  */
 
-app.controller('bookController', function($scope){
-
-})
+app.controller('booksController', function($scope, $http){
+    $http.get('/employeeDetails')
+        .success(function(employee) {
+            console.log(employee);
+            $scope.employee = employee;
+        });
+});
