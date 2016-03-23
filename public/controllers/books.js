7f4a3a42a25a3d5a911183c79dbc34e02d396edf
jadeclan <jberg475@mtroyal.ca>
2016-03-22 20:05:12 -0600

    ToDo using Angular Material

--- public/controllers/books.js
+++ public/controllers/books.js
@@ -1,11 +1,3 @@
 /**
  * Created by Nolan Walker on 2016-03-14.
  */
-
-//app.controller('booksController', function($scope, $http){
-//    $http.get('/employeeDetails')
-//        .success(function(employee) {
-//            console.log(employee);
-//            $scope.employee = employee;
-//        });
-//});
